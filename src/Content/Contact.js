d415906b4d80f1be3bce5252689e0387a74f63a3
PickleJr <picklejr.t.gonzalez@gmail.com>
2018-06-10 01:18:38 -0500

    Updated Form

--- src/Content/Contact.js
+++ src/Content/Contact.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { CSSTransitionGroup } from 'react-transition-group';
 
 import './contact.css';
 
@@ -6,51 +7,145 @@ class Contact extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            name: '',
-            email: '',
-            message: '',
+            postback: false,
+            formInfo: '',
+            form: {
+                name: '',
+                email: '',
+                message: '',
+            },
+            formDementions: {
+                height: "auto",
+                width: "auto",
+            },
+            formHeight: {
+                height: "auto",
+            }
         };
 
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.resetForm = this.resetForm.bind(this);
+    }
+
+    componentDidMount() {
+        let state = this.state;
+        let form = document.querySelector("form");
+        let elemHeight = form.clientHeight + "px";
+        let elemWidth = this.getElementWidth(form) + "px";
+        state.formHeight = {
+            height: elemHeight,
+        };
+        state.formDementions = {
+            height: elemHeight,
+            width: elemWidth,
+        };
+        this.setState(state);
+    }
+
+    getElementWidth(element) {
+        let styles = window.getComputedStyle(element);
+        let padding = parseFloat(styles.paddingLeft) + parseFloat(styles.paddingRight);
+        return element.clientWidth - padding;
     }
 
     handleInputChange(event) {
         const target = event.target;
         const value = target.value;
         const name = target.name;
+        let state = this.state;
+        state.form[name] = value;
 
-        this.setState({
-            [name]: value
-        });
+        this.setState(state);
     }
 
     handleSubmit(event) {
         event.preventDefault();
         const data = new FormData(event.target);
+
+        let newState = this.state;
+        newState.form = {
+            name: '',
+            email: '',
+            message: '',
+        };
+        newState.postback = true;
+        newState.formInfo = 'sending';
+        this.setState(newState);
+
+        fetch("/sendmail", {
+            method: 'POST',
+            body: data,
+        }).catch(err => {
+            console.log(err);
+            let state = this.state;
+            if(state.postback) {
+                state.formInfo = 'error';
+            }
+            this.setState(state);
+        }).then(res => {
+            let state = this.state;
+            if(res.error) {
+                console.log("Error was made, see log");
+                if(state.postback) {
+                    state.formInfo = 'error';
+                }
+            } else {
+                state.formInfo = "Email went through! I'll look at your message soon.";
+            }
+            this.setState(state);
+        });
+    }
+
+    resetForm(event) {
+        let state = this.state;
+        state.postback = false;
+        state.formInfo = '';
+        this.setState(state);
     }
 
     render() {
+        let message = this.state.formInfo;
+        if(message === 'error') {
+            message = (
+                <span id="error">There was an error, please try again or contact me through other means</span>
+            );
+        }
+        let display = (this.state.postback) ? (
+            <div key={2} style={this.state.formDementions} id="postback">
+                <p>{message}</p>
+                <button onClick={this.resetForm}>Reset</button>
+            </div>
+        ) : (
+            <form style={this.state.formDementions} key={1} onSubmit={this.handleSubmit}>
+                <label>
+                    <span>Name:</span>
+                    <input type="text" name="name" placeholder="Your name..." value={this.state.name} onChange={this.handleInputChange} required/>
+                </label>
+                <label>
+                    <span>Email:</span>
+                    <input type="text" name="email" placeholder="Your email address..." value={this.state.email} onChange={this.handleInputChange} required/>
+                </label>
+                <label>
+                    <span>Message:</span>
+                    <textarea name="message" placeholder="Your message..." value={this.state.message} onChange={this.handleInputChange} cols="5" rows="5" required/>
+                </label>
+                <input type="submit" value="Submit"/>
+            </form>
+        );
         return(
             <div className="section">
                 <h2 id="Contact">
                     <span><i className="fab fa-wpforms"></i> Contact Me!</span>
                 </h2>
-                <form onSubmit={this.handleSubmit}>
-                    <label>
-                        <span>Name:</span>
-                        <input type="text" name="name" placeholder="Your name..." value={this.state.name} onChange={this.handleInputChange} required/>
-                    </label>
-                    <label>
-                        <span>Email:</span>
-                        <input type="text" name="email" placeholder="Your email address..." value={this.state.email} onChange={this.handleInputChange} required/>
-                    </label>
-                    <label>
-                        <span>Message:</span>
-                        <textarea name="message" placeholder="Your message..." value={this.state.message} onChange={this.handleInputChange} cols="5" rows="5" required/>
-                    </label>
-                    <input type="submit" value="Submit"/>
-                </form>
+                <div style={this.state.formHeight} id="form">
+                    <CSSTransitionGroup
+                        transitionName="postback"
+                        transitionEnterTimeout={1000}
+                        transitionLeaveTimeout={1000}>
+                        {display}
+                    </CSSTransitionGroup>
+                </div>
             </div>
         );
     }
